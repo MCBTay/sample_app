940a82bffd6f14802c6ff4767110de85544f966b
Bryan Taylor <bryan@bryan-taylor.com>
2011-10-03 17:30:50 -0400

    Done with sign in - Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,14 @@ class User < ActiveRecord::Base
     return user if user.has_password?(submitted_password)
   end
   
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+    # equivalent to 
+    #return nil if user.nil?
+    #return user if user.salt == cookie_salt
+  end
+  
   private
     def encrypt_password
       self.salt = make_salt unless has_password?(password)
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -32,4 +32,24 @@ describe "Users" do
       end
     end
   end
+  
+  describe "sign in/out" do
+    describe "failure" do
+      it "should not sign a user in" do
+      end
+    end
+    
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign Out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
